38408f893ceefc5a54aae7476d603c846bdbeea8
Pegah <43738935+pegahfallah@users.noreply.github.com>
2024-01-08 20:30:05 -0500

    sling model

--- core/src/main/java/com/mysite/core/models/HelloWorldModel.java
+++ core/src/main/java/com/mysite/core/models/HelloWorldModel.java
@@ -18,6 +18,7 @@ package com.mysite.core.models;
 import static org.apache.sling.api.resource.ResourceResolver.PROPERTY_RESOURCE_TYPE;
 
 import javax.annotation.PostConstruct;
+import org.apache.commons.lang3.StringUtils;
 
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -27,16 +28,19 @@ import org.apache.sling.models.annotations.injectorspecific.InjectionStrategy;
 import org.apache.sling.models.annotations.injectorspecific.OSGiService;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 
 import com.day.cq.wcm.api.Page;
 import com.day.cq.wcm.api.PageManager;
 
 import java.util.Optional;
 
-@Model(adaptables = Resource.class)
+@Model(adaptables = Resource.class,
+defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class HelloWorldModel {
 
-    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE, injectionStrategy=InjectionStrategy.OPTIONAL)
+
+    @ValueMapValue(name=PROPERTY_RESOURCE_TYPE)
     @Default(values="No resourceType")
     protected String resourceType;
 
@@ -46,6 +50,12 @@ public class HelloWorldModel {
     private ResourceResolver resourceResolver;
 
     private String message;
+  
+    @ValueMapValue
+    private String title;
+
+    @ValueMapValue
+    private String text;
 
     @PostConstruct
     protected void init() {
@@ -63,4 +73,19 @@ public class HelloWorldModel {
         return message;
     }
 
+    /***
+    *
+    * @return the value of title, if null or blank returns "Default Value here!"
+    */
+    public String getTitle() {
+        return StringUtils.isNotBlank(title) ? title : "Default Value here!";
+    }
+
+    /***
+    *
+    * @return All caps variation of the text value
+    */
+    public String getText() {
+    return StringUtils.isNotBlank(this.text) ? this.text.toUpperCase() : null;
+}
 }
